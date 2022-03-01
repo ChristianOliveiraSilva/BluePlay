cf3ff55b1635346d71a3d3f38075e44912defa8d
ChristianOliveiraSilva <gamesparamundo123@gmail.com>
2022-03-01 16:33:21 -0400

    created first migrations

--- blueplay/settings.py
+++ blueplay/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'playsound.apps.PlaysoundConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- playsound/models.py
+++ playsound/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    name = models.CharField(max_length=200)
+    src = models.CharField(default='',max_length=200)
+    pub_date = models.DateTimeField('date published')
